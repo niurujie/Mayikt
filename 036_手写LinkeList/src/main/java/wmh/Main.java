fdfbe2564cb4d5883974e5966b3733dcad207906
wangmh <wang732276349@163.com>
2019-08-11 00:52:24 +0800

    36 手写LinkedList ### 链表数据底层原理实现：双向链表头尾相接 1.在底层使用静态内部类Node节点存放节点元素，节点元素包含三个信息：关联的上一个节点信息、当前节点值、关联的下一个节点信息 2.add原理：一直通过last在链表结尾添加元素 3.get原理：查询效率低，采用折半的方法查找节点元素 4.remove原理：就是改变节点信息中的头尾信息，然后将删除当前元素值回收

--- 036_手写LinkeList/src/main/java/wmh/Main.java
+++ 036_手写LinkeList/src/main/java/wmh/Main.java
@@ -11,7 +11,7 @@ import java.util.LinkedList;
  **/
 public class Main {
     public static void main(String[] args) {
-        MyList<String> myList=new MyLinkedList();
+        MyList<String> myList=new MyLinkedList<String>();
         myList.add("第一个元素");
         myList.add("第二个元素");
         myList.add("第三个元素");
